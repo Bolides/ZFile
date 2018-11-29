1608d575a73532830f8b9aad1633775d795cd0a6
Stijn Willems <stijn.willems@dooz.be>
2018-11-29 16:30:48 +0100

    Added files

---
+++ ZFile/ZFile-macos/ZFile_macos.h
@@ -0,0 +1,19 @@
+//
+//  ZFile_macos.h
+//  ZFile-macos
+//
+//  Created by Stijn on 29/11/2018.
+//  Copyright © 2018 dooz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZFile_macos.
+FOUNDATION_EXPORT double ZFile_macosVersionNumber;
+
+//! Project version string for ZFile_macos.
+FOUNDATION_EXPORT const unsigned char ZFile_macosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFile_macos/PublicHeader.h>
+
+
