76a57eca3b5a6c50a3ee5d2e7b4e391064dc70dc
Stijn Willems <stijn.willems@dooz.be>
2018-12-22 00:12:10 +0100

    adds ability to run ios zfile tests

---
+++ sources/ZFile/sources/ZFileIOSUseCase/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ZFileIOSUseCase
+//
+//  Created by Stijn on 22/12/2018.
+//  Copyright © 2018 dooz. All rights reserved.
+//
+
+import UIKit
+import ZFile
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
