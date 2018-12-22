cc1b6344dad5a6238b797f99e05b956534b67e0d
Stijn Willems <stijn.willems@dooz.be>
2018-12-22 10:33:05 +0100

    fixes

---
+++ sources/ZFile/sources/FoundationGenericHelper/AnyRawRepresentable.swift
@@ -0,0 +1,22 @@
+//
+//  AnyRawRepresentable.swift
+//  ZFile
+//
+//  Created by Stijn on 22/12/2018.
+//  Copyright © 2018 dooz. All rights reserved.
+//
+
+import Foundation
+
+public struct AnyRawRepresentable<RawValue>: RawRepresentable  {
+    
+    public let rawValue: RawValue
+    
+    public init<R>( _ proxy: R) where R: RawRepresentable, R.RawValue == RawValue {
+        self.rawValue = proxy.rawValue
+    }
+    
+    public init?(rawValue: RawValue) {
+        self.rawValue = rawValue
+    }
+}
---
+++ sources/ZFile/sources/ZFile/BundleProtocol.swift
@@ -0,0 +1,34 @@
+//
+//  BundleProtocol.swift
+//  ZFile
+//
+//  Created by Stijn on 22/12/2018.
+//  Copyright © 2018 dooz. All rights reserved.
+//
+
+import Foundation
+import SourceryAutoProtocols
+import FoundationGenericHelper
+
+// MARK: - Bundle complies to BundleProtocol
+
+public protocol BundleProtocol: AutoMockable {
+    /// sourcery:inline:Bundle.AutoGenerateProtocol
+    
+    func string(for key: AnyRawRepresentable<String>) throws  -> String
+    /// sourcery:end
+}
+
+extension Bundle: BundleProtocol, AutoGenerateProtocol {
+    
+    public enum Error: Swift.Error {
+        case noValueFor(_ key: AnyRawRepresentable<String>)
+    }
+    
+    public func string(for key: AnyRawRepresentable<String>) throws -> String {
+        guard let string = Bundle.main.object(forInfoDictionaryKey: key.rawValue) as? String else {
+            throw Error.noValueFor(key)
+        }
+        return string
+    }
+}
