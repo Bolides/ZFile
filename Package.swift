856075b5ba9aaff7c1b25ed692937009272b0292
doozMen <stijn@dooz.be>
2019-03-13 18:19:14 +0100

    swift package update

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .package(url: "https://github.com/Quick/Quick", from: "1.3.0"),
         .package(url: "https://github.com/Quick/Nimble", from: "7.0.0"),
         .package(url: "https://www.github.com/doozMen/SignPost", from: "1.0.0"),
-        .package(url: "https://www.github.com/doozMen/template-sourcery", from: "1.2.0"),
+        .package(url: "https://www.github.com/dooZDev/template-sourcery", "1.3.3" ..< "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
